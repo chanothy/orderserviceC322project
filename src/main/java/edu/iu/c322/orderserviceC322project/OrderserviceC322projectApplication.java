58ae35d85e0d0b4196059ccc72c3473408030594
timchan <timchan@iu.edu>
2023-04-05 19:23:03 -0400

    Initial commit

---
+++ src/main/java/edu/iu/c322/orderserviceC322project/OrderserviceC322projectApplication.java
@@ -0,0 +1,13 @@
+package edu.iu.c322.orderserviceC322project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderserviceC322projectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderserviceC322projectApplication.class, args);
+	}
+
+}
